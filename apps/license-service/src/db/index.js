80bbb79633b75a4445a0aa8e318fdc3ab8c66b8f
Thomas Kjellberg <thomas.kjellberg@rebelandbird.com>
2023-06-28 10:53:51 +0200

    Use env vars

--- apps/license-service/src/db/index.js
+++ apps/license-service/src/db/index.js
@@ -1,9 +1,15 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 import mongoose from 'mongoose'
 import checkLicenses from './check-licenses.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB } =
+  process.env
 
 async function connectToDb() {
   await mongoose.connect(
-    'mongodb+srv://Cluster72424:THBnQlxYTGx9@cluster72424.nfsljvr.mongodb.net/licensify?appName=mongosh+1.10.1',
+    `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB}?appName=mongosh+1.10.1`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
 }
